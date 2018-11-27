1d5049629c0eaed0b05d9ed231119460fe8b6d98
A. Shuryhin <a.shuryhin@godeltech.com>
2018-11-27 16:30:33 +0300

    Added slf4j logger

--- Session101/src/main/java/edu/local/java/core/session101/singleton/enumx/SingletonEnumTest.java
+++ Session101/src/main/java/edu/local/java/core/session101/singleton/enumx/SingletonEnumTest.java
@@ -1,8 +1,16 @@
 package edu.local.java.core.session101.singleton.enumx;
 
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SingletonEnumTest {
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
 	public static void main(String[] args) {
+		LOGGER.info("Logger: '" + SingletonEnum.INSTANCE.sayHello());
 		System.out.println(SingletonEnum.INSTANCE.sayHello());
 	}
 }
